8ece1608a7a7bed31fc2be8d3ac6ee23f387e176
oscarcool <oscarcool@rambler.ru>
2022-05-17 13:09:12 +0300

    autologin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,21 @@
 class UsersController < ApplicationController
   def create
     user_params = params.require(:user).permit(:name, :nickname, :email, :password)
+    @user = User.new(user_params)
 
-    User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
 
-    redirect_to root_path, notice: "Вы успешно зарегистрировались!"
+      redirect_to root_path, notice: "Вы успешно зарегистрировались!"
+    else
+      flash.now[:alert] = "Вы неправильно заполнили поля формы регистрации!"
+
+      render "new"
+    end
   end
 
   def new
+    session[:current_time] = Time.now
+    @user = User.new
   end
 end
