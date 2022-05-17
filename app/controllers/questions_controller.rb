c2211856c44099ebfa860177e09fb570717afd3f
oscarcool <oscarcool@rambler.ru>
2022-05-17 11:35:48 +0300

    added some actions and views

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,6 +8,14 @@ class QuestionsController < ApplicationController
     @question.destroy
   end
 
+  def index
+    @questions = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
   def update
     @question = Question.find(params[:id])
     @question.update(body: params[:question][:body],user_id: params[:question][:user_id])
