8abdb655c6d2b39216a474fa50b70355081b53af
oscarcool <oscarcool@rambler.ru>
2022-05-17 14:17:54 +0300

    added simple_format

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_17_105910) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_17_111208) do
   create_table "questions", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_17_105910) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "navbar_color", default: "#866363"
+    t.index ["nickname"], name: "index_users_on_nickname", unique: true
   end
 
 end
