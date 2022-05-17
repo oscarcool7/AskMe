7ffe1cc81a208a746492263c6e5b7ccb4c2f202c
oscarcool <oscarcool@rambler.ru>
2022-05-17 13:53:36 +0300

    validation email, nickname and question

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
+  VALID_EMAIL = /\A[\w\d]+@[\w\d]+\.[\w]+/
+  VALID_NICKNAME = /\A\w+\z/
+
   has_secure_password
 
   before_save :downcase_nickname
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL }
+  validates :nickname, presence: true, uniqueness: true, length: { maximum: 40 }, format: { with: VALID_NICKNAME }
 
   private
 
